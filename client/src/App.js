ea39673d2df35a3327bf85ee7ed54d890add0818
Dinuka-Kaveen <dinukadominguhewa@gmail.com>
2023-01-29 16:10:08 +0530

    get posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      allPosts:[] //allPosts Array
+    };
+  }
+
+  //execute retrievePosts() method
+  componentDidMount(){
+    this.retrievePosts();
+  }
+
+  retrievePosts(){
+    axios.get("http://localhost:8000/posts").then(res => {
+      if(res.data.success){
+        this.setState({
+          allPosts:res.data.existingPosts
+        });
+        console.log(this.state.allPosts);
+      }
+    })
+  }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        {this.state.allPosts.map(posts => (
+          <div>
+            <p>{posts.topic}</p>
+            <p>{posts.description}</p>
+            <p>{posts.postCategory}</p>
+          </div>
+        ))}
+      </div>
     )
   }
 }
