49af624487e38125afac86770ae27d1131165d6f
Dinuka-Kaveen <dinukadominguhewa@gmail.com>
2023-01-30 21:30:28 +0530

    zdfsdf

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -17,7 +17,7 @@ export default class Home extends Component {
   }
 
   retrievePosts() {
-    axios.get("http://localhost:8000/posts").then((res) => {
+    axios.get("/posts").then((res) => {
       if (res.data.success) {
         this.setState({
           allPosts: res.data.existingPosts,
