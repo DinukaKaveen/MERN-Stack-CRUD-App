60d91b02d10198c2173026df2114f835c058b830
Dinuka-Kaveen <dinukadominguhewa@gmail.com>
2023-01-30 18:42:38 +0530

    update navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import CreatePost from './components/CreatePost';
-import EditPost from './components/EditPost';
-import Home from './components/Home';
-import NavBar from './components/NavBar';
-import PostDetails from './components/PostDetails';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreatePost from "./components/CreatePost";
+import EditPost from "./components/EditPost";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import PostDetails from "./components/PostDetails";
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <div>
-          <Switch>
-            <NavBar/>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/add" component={CreatePost}></Route>
-            <Route path="/edit/:id" component={EditPost}></Route>
-            <Route path="/post/:id" component={PostDetails}></Route>
-          </Switch>
+          <NavBar />
+          <Routes>
+            <Route path="/" exact element={<Home/>}></Route>
+            <Route path="/add" element={<CreatePost/>}></Route>
+            <Route path="/edit/:id" element={<EditPost/>}></Route>
+            <Route path="/post/:id" element={<PostDetails/>}></Route>
+          </Routes>
         </div>
-      </Router>
-    )
+      </BrowserRouter>
+    );
   }
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,50 +1,37 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              Navbar
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Features
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Pricing
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled">Disabled</a>
-                </li>
-              </ul>
-            </div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/add" className="nav-link">Create Post</Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
