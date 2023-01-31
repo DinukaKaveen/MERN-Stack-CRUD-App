493da5382c8682dd27e3077ca904fb44217b76a0
Dinuka-Kaveen <dinukadominguhewa@gmail.com>
2023-01-31 12:18:28 +0530

    dfasf

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PostDetails extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      posts: {}
+    };
+  }
+
   render() {
     return (
       <div>PostDetails</div>
