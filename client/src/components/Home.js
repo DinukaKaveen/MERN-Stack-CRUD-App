d2836786c699ad2f7fd08d90bddd1612bc700553
Dinuka-Kaveen <dinukadominguhewa@gmail.com>
2023-01-31 11:19:56 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -30,7 +30,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <table class="table">
+        <table className="table">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -40,7 +40,8 @@ export default class Home extends Component {
               <th scope="col">Action</th>
             </tr>
           </thead>
-          <tbody class="table-group-divider">
+          <tbody className="table-group-divider">
+
             {this.state.allPosts.map((posts, index) => (
               <tr key={index}>
                 <th scope="row">{index + 1}</th>
@@ -53,15 +54,16 @@ export default class Home extends Component {
                   </Link>
                   &nbsp;
                   <Link to={`/edit/${posts._id}`} type="button" className="btn btn-outline-warning">
-                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i className="fa-solid fa-pen-to-square"></i>
                   </Link>
                   &nbsp;
                   <Link to="" type="button" className="btn btn-outline-danger">
-                    <i class="fa-solid fa-trash"></i>
+                    <i className="fa-solid fa-trash"></i>
                   </Link>
                 </td>
               </tr>
             ))}
+            
           </tbody>
         </table>
       </div>
